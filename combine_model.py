8fb37b89ec7f0acafe29c2b0d3ebb8184da798b4
Anshukumar123975 <146181349+Anshukumar123975@users.noreply.github.com>
2024-11-23 17:35:42 +0530

    Update combine_model.py

--- combine_model.py
+++ combine_model.py
@@ -42,4 +42,4 @@ def analyze_message():
         return jsonify({'allowed': True, 'reason': 'Unsupported language'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001)
